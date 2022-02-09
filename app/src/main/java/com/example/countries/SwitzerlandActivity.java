643a6c468538904c9d0456e907df5daf3221afc9
1111 <hasti.m.lse.bbb@gmail.com>
2022-02-09 17:40:28 +0330

    making link and making 3 Activities

--- app/src/main/java/com/example/countries/SwitzerlandActivity.java
+++ app/src/main/java/com/example/countries/SwitzerlandActivity.java
@@ -10,9 +10,11 @@ import android.os.Bundle;
 
 import com.bumptech.glide.Glide;
 
+import java.io.PipedInputStream;
+
 public class SwitzerlandActivity extends AppCompatActivity {
 
-    private AppCompatTextView Nestlé, roche, novartis, chubb;
+    AppCompatTextView Nestle, roche, novartis, chubb;
 
 
     @Override
@@ -21,45 +23,19 @@ public class SwitzerlandActivity extends AppCompatActivity {
         setContentView(R.layout.activity_switzerland);
 
 
-        Nestlé = findViewById(R.id.Nestlé);
-        Nestlé.setOnClickListener(v -> startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.google.com/url?sa=t&rct=j&q=&esrc=s&source=web&cd=&cad=rja&uact=8&ved=2ahUKEwiRx6H30eP1AhXRyIUKHYYSDqoQFnoECDoQAQ&url=https%3A%2F%2Fwww.nestle.com%2Faboutus&usg=AOvVaw3jcmdFgRlFSaRMJtkq7eMp"))));
+        Nestle = findViewById(R.id.Nestle);
+        Nestle.setOnClickListener(v -> startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse(PicLinks.NESTLE_URL))));
 
         roche = findViewById(R.id.roche);
-        roche.setOnClickListener(v -> startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.google.com/url?sa=t&rct=j&q=&esrc=s&source=web&cd=&cad=rja&uact=8&ved=2ahUKEwiAsu-J0uP1AhXQyIUKHTf7DWIQFnoECCsQAQ&url=https%3A%2F%2Fwww.roche.com%2Fabout.htm&usg=AOvVaw3WnVDPDKDsLMJ0A6_M0-Ep"))));
+        roche.setOnClickListener(v -> startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse(PicLinks.ROCHE_URL))));
 
         novartis = findViewById(R.id.novartis);
-        novartis.setOnClickListener(v -> startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.google.com/url?sa=t&rct=j&q=&esrc=s&source=web&cd=&cad=rja&uact=8&ved=2ahUKEwiDjIii0uP1AhUJ4YUKHTSIBxkQtwJ6BAgIEAM&url=https%3A%2F%2Fwww.novartis.com%2Fabout&usg=AOvVaw3gTzDQtR_jWCynznnIcl5U"))));
+        novartis.setOnClickListener(v -> startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse(PicLinks.NAVARIST_URL))));
 
         chubb = findViewById(R.id.chubb);
-        chubb.setOnClickListener(v -> startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.google.com/url?sa=t&rct=j&q=&esrc=s&source=web&cd=&cad=rja&uact=8&ved=2ahUKEwje47Cr0uP1AhUJ3hoKHbARDlsQtwJ6BAgFEAM&url=https%3A%2F%2Fwww.chubb.com%2Fus-en%2Fabout-chubb%2Fwho-we-are.aspx&usg=AOvVaw3qVQBzcYVRAA1ikFoLdAt5"))));
+        chubb.setOnClickListener(v -> startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse(PicLinks.CHUB_URL))));
 
 
-        AppCompatImageView train = findViewById(R.id.train_switzerland);
-        Glide.with(this).load("https://images.pexels.com/photos/773471/pexels-photo-773471.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500").placeholder(R.drawable.ic_launcher_background).error(R.drawable.broken_image).into(train);
-
-        AppCompatImageView nature = findViewById(R.id.nature_switzerland);
-        Glide.with(this).load("https://images.pexels.com/photos/922978/pexels-photo-922978.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500").placeholder(R.drawable.ic_launcher_background).error(R.drawable.broken_image).into(nature);
-
-        AppCompatImageView clouds = findViewById(R.id.clouds_switzerland);
-        Glide.with(this).load("https://images.pexels.com/photos/415958/pexels-photo-415958.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500").placeholder(R.drawable.ic_launcher_background).error(R.drawable.broken_image).into(clouds);
-
-        AppCompatImageView bridge = findViewById(R.id.bridge_switzerland);
-        Glide.with(this).load("https://images.pexels.com/photos/2762921/pexels-photo-2762921.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500").placeholder(R.drawable.ic_launcher_background).error(R.drawable.broken_image).into(bridge);
-
-        AppCompatImageView homes = findViewById(R.id.homes_switzerland);
-        Glide.with(this).load("https://images.unsplash.com/photo-1605825831039-8b6b4199b04a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8aW50ZXJsYWtlbnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60").placeholder(R.drawable.ic_launcher_background).error(R.drawable.broken_image).into(homes);
-
-        AppCompatImageView nesdoa = findViewById(R.id.nesdoa_Switzerland);
-        Glide.with(this).load("https://worldscholarshipforum.com/wp-content/uploads/2020/10/University-of-Zurich.jpg").placeholder(R.drawable.ic_launcher_background).error(R.drawable.broken_image).into(nesdoa);
-
-        AppCompatImageView damalop = findViewById(R.id.dmalop_Switzerland);
-        Glide.with(this).load("https://euroscholars.eu/images/made/64b5413671794cb5/unige_28674_840_500_s_c1_c_c.jpeg").placeholder(R.drawable.ic_launcher_background).error(R.drawable.broken_image).into(damalop);
-
-        AppCompatImageView naloki = findViewById(R.id.naloki_Switzerland);
-        Glide.with(this).load("https://www.wur.nl/upload/a7705f68-d597-439c-ac73-d5a854cda6c1_1.jpg").placeholder(R.drawable.ic_launcher_background).error(R.drawable.broken_image).into(naloki);
-
-        AppCompatImageView laniokd = findViewById(R.id.laniokd_Switzerland);
-        Glide.with(this).load("https://scholarshipsforafricans.com/wp-content/uploads/2021/09/university-of-lausanne.jpg").placeholder(R.drawable.ic_launcher_background).error(R.drawable.broken_image).into(laniokd);
 
     }
 }
--- app/src/main/java/com/example/countries/ThailandActivity.java
+++ app/src/main/java/com/example/countries/ThailandActivity.java
@@ -13,7 +13,7 @@ import com.bumptech.glide.Glide;
 
 public class ThailandActivity extends AppCompatActivity {
 
-    private AppCompatTextView ptt, cpAll, siam, thai, skanska;
+     AppCompatTextView ptt, cpAll, siam, thai, skanska;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,47 +21,21 @@ public class ThailandActivity extends AppCompatActivity {
         setContentView(R.layout.activity_thailand);
 
         ptt = findViewById(R.id.ptt);
-        ptt.setOnClickListener(v -> startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.google.com/url?sa=t&rct=j&q=&esrc=s&source=web&cd=&cad=rja&uact=8&ved=2ahUKEwjFxY2-1eP1AhUJLBoKHU9kB2kQFnoECAoQAQ&url=https%3A%2F%2Fwww.pttplc.com%2Fen%2FAbout.aspx&usg=AOvVaw1QyVOlH0AS14TI1fgxR56_"))));
+        ptt.setOnClickListener(v -> startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse(PicLinks.PTT_URL))));
 
         cpAll = findViewById(R.id.cp_all);
-        cpAll.setOnClickListener(v -> startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.google.com/url?sa=t&rct=j&q=&esrc=s&source=web&cd=&cad=rja&uact=8&ved=2ahUKEwiouqvG1eP1AhUCx4UKHV1FAP0QFnoECFAQAQ&url=https%3A%2F%2Fwww.cpall.co.th%2Fen%2Fcompany-information&usg=AOvVaw3KmtDCDu3_b9k9ESvJUm3l"))));
+        cpAll.setOnClickListener(v -> startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse(PicLinks.CP_URL))));
 
         siam = findViewById(R.id.siam);
-        siam.setOnClickListener(v -> startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.google.com/url?sa=t&rct=j&q=&esrc=s&source=web&cd=&cad=rja&uact=8&ved=2ahUKEwiLte7f1eP1AhUMyYUKHYXLDhYQFnoECAUQAw&url=https%3A%2F%2Fwww.siam.org%2Fabout-siam&usg=AOvVaw2vUmaSPKBd8ZBkif2ENFPL"))));
+        siam.setOnClickListener(v -> startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse(PicLinks.SIAM_URL))));
 
         thai = findViewById(R.id.thai);
-        thai.setOnClickListener(v -> startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.google.com/url?sa=t&rct=j&q=&esrc=s&source=web&cd=&cad=rja&uact=8&ved=2ahUKEwjV58Xn1eP1AhUix4UKHfiWBXsQFnoECB8QAQ&url=https%3A%2F%2Fwww.thaiairways.com%2Fen_IE%2Fabout_thai%2Fcompany_profile%2Findex.page%3F&usg=AOvVaw3vEmCKC1aYQQNJYnbhkDir"))));
+        thai.setOnClickListener(v -> startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse(PicLinks.THAI_URL))));
 
         skanska = findViewById(R.id.skanska);
-        skanska.setOnClickListener(v -> startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.google.com/url?sa=t&rct=j&q=&esrc=s&source=web&cd=&cad=rja&uact=8&ved=2ahUKEwjG3_721eP1AhUD14UKHdNGCCUQFnoECEYQAQ&url=https%3A%2F%2Fwww.usa.skanska.com%2Fwho-we-are%2Fabout-skanska%2F&usg=AOvVaw0IzMZxdh9X8zREhMp0PwsF"))));
+        skanska.setOnClickListener(v -> startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse(PicLinks.SHANSKAMA_URL))));
 
 
-        AppCompatImageView river = findViewById(R.id.river_thailand);
-        Glide.with(this).load("https://images.pexels.com/photos/2108831/pexels-photo-2108831.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500").placeholder(R.drawable.ic_launcher_background).error(R.drawable.broken_image).into(river);
-
-        AppCompatImageView bridge = findViewById(R.id.bridge_thailand);
-        Glide.with(this).load("https://images.pexels.com/photos/753885/pexels-photo-753885.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500").placeholder(R.drawable.ic_launcher_background).error(R.drawable.broken_image).into(bridge);
-
-        AppCompatImageView fish = findViewById(R.id.fish_thailand);
-        Glide.with(this).load("https://images.pexels.com/photos/3538236/pexels-photo-3538236.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500").placeholder(R.drawable.ic_launcher_background).error(R.drawable.broken_image).into(fish);
-
-        AppCompatImageView shop = findViewById(R.id.shop_thailand);
-        Glide.with(this).load("https://media.istockphoto.com/photos/sunday-market-walking-street-picture-id613547878?k=20&m=613547878&s=612x612&w=0&h=OdjPVGtKtbsDaf3zt4m2GV0SFkmG1yhmsZQ2uVqXJ3s=").placeholder(R.drawable.ic_launcher_background).error(R.drawable.broken_image).into(shop);
-
-        AppCompatImageView castle = findViewById(R.id.castle_thailand);
-        Glide.with(this).load("https://images.pexels.com/photos/460376/pexels-photo-460376.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500").placeholder(R.drawable.ic_launcher_background).error(R.drawable.broken_image).into(castle);
-
-        AppCompatImageView jiandk = findViewById(R.id.jiandk_thailand);
-        Glide.with(this).load("https://www.bu.ac.th/uploads/faculty/contact/20170915153322_s0Vdj5MSXnzbBe1_QDWMgaFyGWvhtzR.jpg").placeholder(R.drawable.ic_launcher_background).error(R.drawable.broken_image).into(jiandk);
-
-        AppCompatImageView naklspe = findViewById(R.id.naaklspe_thailand);
-        Glide.with(this).load("https://www.chula.ac.th/wp-content/uploads/2018/01/chula-faculty-arts-hero-desktop-1024x640.jpg").placeholder(R.drawable.ic_launcher_background).error(R.drawable.broken_image).into(naklspe);
-
-        AppCompatImageView naolam = findViewById(R.id.naolam_thailand);
-        Glide.with(this).load("http://www.rocapply.com/assets/pictures/universities/San%20Francisco/Prince-of-songkla-header.jpg").placeholder(R.drawable.ic_launcher_background).error(R.drawable.broken_image).into(naolam);
-
-        AppCompatImageView nsusna = findViewById(R.id.nsusna_thailand);
-        Glide.with(this).load("https://9968c6ef49dc043599a5-e151928c3d69a5a4a2d07a8bf3efa90a.ssl.cf2.rackcdn.com/672578.jpg").placeholder(R.drawable.ic_launcher_background).error(R.drawable.broken_image).into(nsusna);
 
     }
 }
